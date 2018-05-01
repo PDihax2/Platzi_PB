01e06c47746afaac0afa77b01458105f38ae927b
PDihax <pedro.lopez030200@gmail.com>
2018-05-01 17:09:48 -0300

    hasta clase 20

--- villaplatzi/platzi.js
+++ villaplatzi/platzi.js
@@ -1,25 +1,95 @@
 let vp = document.getElementById("villaplatzi");
 let papel = vp.getContext("2d");
-let mapa = "tile.png";
-let fondo = new Image();
-fondo.src = mapa;
-fondo.addEventListener("load", dibujar);
+let cantidad = aleatorio(5, 25);
 
-let vaca = new Image();
-vaca.src = "vaca.png";
-vaca.addEventListener("load", );
+let fondo = {
+  url: "tile.png",
+  cargaOK: false
+};
 
-let cerdo = new Image();
-cerdo.src = "cerdo,png";
-vaca.addEventListener("load", );
+let vaca = {
+  url: "vaca.png",
+  cargaOK: false
+};
 
-let pollo = new Image();
-pollo.src = "pollo.png";
-vaca.addEventListener("load", );
+let cerdo = {
+  url: "cerdo.png",
+  xPos : 250,
+  yPos : 250,
+  salto : 20,
+  cargaOK: false
+};
 
+let teclas = {
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40
+};
+
+
+fondo.imagen = new Image();
+fondo.imagen.src = fondo.url;
+fondo.imagen.addEventListener("load", cargarFondo);
+
+vaca.imagen = new Image();
+vaca.imagen.src = vaca.url;
+vaca.imagen.addEventListener("load", cargarVacas);
+
+cerdo.imagen = new Image();
+cerdo.imagen.src = cerdo.url;
+cerdo.imagen.addEventListener("load", cargarCerdo);
+
+document.addEventListener("keydown", moverCerdo);
+
+
+function moverCerdo(e) {
+  if (e.keyCode ==  teclas.UP) {
+    cerdo.yPos -= cerdo.salto;
+  }
+  if (e.keyCode ==  teclas.DOWN) {
+    cerdo.yPos += cerdo.salto;
+  }
+  if (e.keyCode ==  teclas.LEFT) {
+    cerdo.xPos -= cerdo.salto;
+  }
+  if (e.keyCode ==  teclas.RIGHT) {
+    cerdo.xPos += cerdo.salto;
+  }
+  dibujar();
+}
+
+function cargarFondo() {
+  fondo.cargaOK = true;
+  dibujar();
+}
+
+function cargarVacas() {
+  vaca.cargaOK = true;
+  dibujar();
+}
+
+function cargarCerdo() {
+  cerdo.cargaOK = true;
+  dibujar();
+}
 
 function dibujar() {
-  papel.drawImage(fondo,0, 0);
+  if (fondo.cargaOK) {
+    papel.drawImage(fondo.imagen, 0, 0);
+  }
+  if (vaca.cargaOK) {
+    for (let i = 0; i < cantidad; i++) {
+      let x = aleatorio(0, 7);
+      let y = aleatorio(0, 7);
+      x *= 60;
+      y *= 60;
+      papel.drawImage(vaca.imagen, x , y);
+    }
+  }
+  if (cerdo.cargaOK) {
+    papel.drawImage(cerdo.imagen, cerdo.xPos, cerdo.yPos);
+  }
 }
 
 function aleatorio(min, max) {
